ac7a1519f2fe6e187d61b29702ae0e0dae3d28f9
zahmad004 <z@zahmad.com>
2016-12-13 01:31:15 -0500

    z

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ZZZZ</title>
+	<title>The Nest Intake form</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 </head>
@@ -12,7 +12,7 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello Z!"; ?></h1>
+				<h1 id = "message"><?php echo "Intake Form"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
